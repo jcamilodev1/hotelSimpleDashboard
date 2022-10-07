f4f0b77fefd19917173e4fb4c82287b06b3bb368
camilo garcia <camilo456garcia@gmail.com>
2022-10-07 13:26:30 -0500

    mock habitaciones

---
+++ src/mocks/handlers.js
@@ -0,0 +1,49 @@
+import { rest } from "msw";
+export const handlers = [
+  rest.get("/habitaciones", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 1,
+          cantidad: 3,
+          bookings: [
+            { start: "2022-10-01", end: "2022-10-05" },
+            { start: "2022-10-05", end: "2022-01-08" },
+            { start: "2022-11-02", end: "2022-01-05" },
+          ],
+        },
+        {
+          id: 2,
+          cantidad: 4,
+          "bookings ": [
+            { start: "2022-10-01", end: "2022-10-05" },
+            { start: "2022-11-15", end: "2022-01-18" },
+            { start: "2022-11-10", end: "2022-11-15" },
+          ],
+        },
+        {
+          id: 3,
+          cantidad: 2,
+          "bookings ": [
+            { start: "2022-10-01", end: "2022-10-05" },
+            { start: "2022-11-05", end: "2022-01-08" },
+            { start: "2022-11-01", end: "2022-11-05" },
+          ],
+        },
+        {
+          id: 4,
+          cantidad: 2,
+          "bookings ": [
+            { start: "2022-10-01", end: "2022-10-05" },
+            { start: "2022-11-05", end: "2022-01-08" },
+            { start: "2022-11-01", end: "2022-11-05" },
+          ],
+        },
+      ])
+    );
+  }),
+  rest.post("/habitaciones", (req, res, ctx) => {
+    return res(ctx.json({ success: true }));
+  }),
+];
