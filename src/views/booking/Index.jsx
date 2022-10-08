27616d1a0e3a377f57e39600e33df1a4d50841cc
camilo garcia <camilo456garcia@gmail.com>
2022-10-07 23:24:56 -0500

    dele and modify hooks to upgrade function

--- src/views/booking/Index.jsx
+++ src/views/booking/Index.jsx
@@ -1,44 +1,58 @@
 import React, { useEffect, useState } from "react";
 import './Index.css';
 
-import { useForm } from "react-hook-form";
-
+import { useForm, reset  } from "react-hook-form";
+import Swal from 'sweetalert2'
 const Booking = () => {
   let date = new Date()
   let day = date.getDate()
   let month = date.getMonth() + 1
   let year = date.getFullYear()
 
-  const [habitaciones, setHabitaciones] = useState([]);
-  const [habitacionesDisponible, setHabitacionesDisponible] = useState([]);
-
+  let habitaciones
+  let habitacionesDisponible
   const getRoom = (data) => {
     let result = []
     habitaciones.some(element => {
       if (data.cantidad <= element.cantidad) {
         result = element.bookings.filter(word => (data.start <= word.start) && (word.start <= data.end));
-        console.log(result.length)
-        if(result.length === 0){ 
-          setHabitacionesDisponible(element)
+        if (result.length === 0) {
+          habitacionesDisponible = element
+        }
+        else{
+          habitacionesDisponible = []
         }
       }
       return (result.length === 0)
     });
   }
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => {
-    getRoom(data)
+  const onSubmit =async  (data, e) => {
+    habitaciones = await getRooms()
+    getRoom(await data)
     console.log(habitacionesDisponible)
+    if(habitacionesDisponible.length === 0){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'La habitacion ya esta ocupada para tus fechas',
+      })
+    }else{
+      Swal.fire(
+        'Tu solicitud ha sido exitosa',
+        `Reservaste la habitacion con id: ${habitacionesDisponible.id}`,
+        'success'
+      )
+    }
+    habitacionesDisponible = []
+    e.target.reset();
   };
+  const getRooms = async () => {
+    const result = await fetch('/habitaciones')
+    const response = await result.json()
+    return response
+  }
 
-
-
-  useEffect(() => {
-    fetch('/habitaciones')
-      .then(response => response.json())
-      .then(json => setHabitaciones(json))
-      .catch(error => console.log(error));
-  }, []);
   return (
     <>
       <h1 className=''>Bienvenidos al hotel </h1>
@@ -58,10 +72,9 @@ const Booking = () => {
         </div>
         <div className="form-group">
           <label for="edad">Fecha</label>
-          ${day < 10 ? `0${day}` : `${day}`}
           <div className='date'>
             <input type="date" className="form-control" min={`${year}-${month < 10 ? `0${month}` : `${month}`}-${day < 10 ? `0${day}` : `${day}`}`} max={`${year}-12-31`} {...register("start", { required: true })} required />
-            <input type="date" className="form-control" min={`${year}-${month < 10 ? `0${month}` : `${month}`}-${day+1 < 10 ? `0${day+1}` : `${day+1}`}`} max={`${year}-12-31`} {...register("end", { required: true })} required />
+            <input type="date" className="form-control" min={`${year}-${month < 10 ? `0${month}` : `${month}`}-${day + 1 < 10 ? `0${day + 1}` : `${day + 1}`}`} max={`${year}-12-31`} {...register("end", { required: true })} required />
           </div>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
